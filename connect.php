33f8ab84e77caf349d2fa2e248d605179682ced7
valkonx98 <38484838+valkonx98@users.noreply.github.com>
2018-04-18 10:01:27 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$db_host = 'localhost';
+$db_user
+$db_password='tiger';
+$db_name='2015cse115';
+
+$dbh==myqli_connect($db_host,$db_user,$db_password,$db_name)or die("error connecting to the database".mysqli_connect_error($dbh));
+?>
